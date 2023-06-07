cf621c5a568acdda6b22f8a00cf4b861dc432e50
hayatbangash <hayatbangash66@gmail.com>
2023-06-07 15:11:38 +0500

    my product => converted to mvvm

--- ism_mart/lib/exports/export_seller.dart
+++ ism_mart/lib/exports/export_seller.dart
@@ -2,7 +2,6 @@ export '../screens/membership/member_ship_view.dart';
 export '../screens/my_orders/my_orders_view.dart';
 export '../widgets/single_recent_orders_item.dart';
 export '../screens/add_product/add_product_view.dart';
-export '../screens/my_products/my_products_view.dart';
 export '../screens/seller_dashboard/seller_dashboard_view.dart';
 export '../screens/seller_home/seller_home_view.dart';
 export '../screens/update_vendor/register_seller_view.dart';
