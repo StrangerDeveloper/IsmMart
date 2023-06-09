ce6934d05c1395f251b6219361f409b2ac1cea67
hayatbangash <hayatbangash66@gmail.com>
2023-06-09 09:47:27 +0500

    removed unused code...

--- ism_mart/lib/exports/export_settings.dart
+++ ism_mart/lib/exports/export_settings.dart
@@ -1,5 +1,4 @@
 export '../screens/faq/faq_view.dart';
 export '../screens/general_setting/general_setting_view.dart';
-export '../screens/notification/notification_view.dart';
 export '../screens/setting/settings_view.dart';
 export '../widgets/custom_header.dart';
