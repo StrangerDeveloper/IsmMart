7e18c4a939b444df98a82bef2e6c5227c22537c7
hayatbangash <hayatbangash66@gmail.com>
2023-06-12 17:38:07 +0500

    seller dashboard -- added recent order api!

--- ism_mart/lib/exports/export_seller.dart
+++ ism_mart/lib/exports/export_seller.dart
@@ -1,6 +1,5 @@
 export '../widgets/single_recent_orders_item.dart';
 export '../screens/add_product/add_product_view.dart';
-export '../screens/seller_dashboard/seller_dashboard_view.dart';
 export '../screens/seller_home/seller_home_view.dart';
 export '../widgets/single_order_details_ui.dart';
 export '../screens/update_products/update_product_view.dart';
