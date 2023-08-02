2a24de78f48eb84a7cf763d9907e139d9bd04b54
strangerdeveloper <strangerdeveloper081@gmail.com>
2023-08-02 15:16:07 +0500

    modification before build

--- ism_mart/lib/screens/dashboard/dashboard_view.dart
+++ ism_mart/lib/screens/dashboard/dashboard_view.dart
@@ -9,7 +9,6 @@ import 'package:ism_mart/exports/exports_model.dart';
 import 'package:ism_mart/screens/top_vendors/top_vendors_model.dart';
 import 'package:ism_mart/exports/exports_utils.dart';
 import 'package:ism_mart/helper/languages/translations_key.dart' as langKey;
-import 'package:upgrader/upgrader.dart';
 import '../../helper/global_variables.dart';
 import '../../widgets/custom_grey_border_container.dart';
 import '../../widgets/custom_loading.dart';
