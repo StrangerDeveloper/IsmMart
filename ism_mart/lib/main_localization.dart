9e7a0d34480746a89c1a28bf24c016ba3569019a
strangerdeveloper <strangerdeveloper081@gmail.com>
2023-08-16 15:23:50 +0500

    translations changes

--- ism_mart/lib/main_localization.dart
+++ ism_mart/lib/main_localization.dart
@@ -3,16 +3,16 @@
 // import 'dart:convert';
 // import 'package:http/http.dart' as http;
 // import 'package:csv/csv.dart';
-//
+
 // void main() {
 //   updateLocalizationFile();
 // }
-//
+
 // Future updateLocalizationFile() async {
 //   //the document id for your google sheet
 //   String documentId = "1KOwB8NH_O3UvoIuWweapOL_gbxEfEkGSqD9wPjJSBpA";
 //   //the sheetid of your google sheet
-//
+
 // // SheetID
 // //      0-->General and All app widget Text
 // //      15336104---> General Single Data (Sheet4)
@@ -26,9 +26,9 @@
 // //      1563929016 ---> hayat
 // //      15336104 ----> Sheet4
 // //      1522681252 ---> VendorTermsAndConditions
-//
-//   String sheetId = "15336104";
-//
+
+//   String sheetId = "1563929016";
+
 //   String _phraseKey = '';
 //   List<LocalizationModel> _localizations = [];
 //   String _localizationFile = """import 'package:get/get.dart';
@@ -36,39 +36,39 @@
 //   @override
 //   Map<String, Map<String, String>> get keys => {
 //     """;
-//
+
 //   try {
 //     final url =
 //         'https://docs.google.com/spreadsheets/d/$documentId/export?format=csv&id=$documentId&gid=$sheetId';
-//
+
 //     stdout.writeln('');
 //     stdout.writeln('---------------------------------------');
 //     stdout.writeln('Downloading Google sheet url "$url" ...');
 //     stdout.writeln('---------------------------------------');
 //     var response = await http
 //         .get(Uri.parse(url), headers: {'accept': 'text/csv;charset=UTF-8'});
-//
+
 //     //print('Google sheet csv:\n ${response.body}');
-//
+
 //     final bytes = response.bodyBytes.toList();
 //     final csv = Stream<List<int>>.fromIterable([bytes]);
-//
+
 //     final fields = await csv
 //         .transform(utf8.decoder)
 //         .transform(CsvToListConverter(
 //           shouldParseNumbers: false,
 //         ))
 //         .toList();
-//
+
 //     final index = fields[0]
 //         .cast<String>()
 //         .map(_uniformizeKey)
 //         .takeWhile((x) => x.isNotEmpty)
 //         .toList();
-//
+
 //     for (var r = 1; r < fields.length; r++) {
 //       final rowValues = fields[r];
-//
+
 //       /// Creating a map
 //       final row = Map<String, String>.fromEntries(
 //         rowValues
@@ -81,7 +81,7 @@
 //               (e) => MapEntry(index[e.key], e.value),
 //             ),
 //       );
-//
+
 //       row.forEach((key, value) {
 //         if (key == 'key') {
 //           _phraseKey = value;
@@ -101,7 +101,7 @@
 //         }
 //       });
 //     }
-//
+
 //     _localizations.forEach((_localization) {
 //       String _language = _localization.language;
 //       String _currentLanguageTextCode = "'$_language': {\n";
@@ -120,7 +120,7 @@
 //       }
 //       """;
 //     _localizationFile = _localizationFile + _fileEnding;
-//
+
 //     stdout.writeln('');
 //     stdout.writeln('---------------------------------------');
 //     //stdout.writeln('Saving app_translation_new.g.dart');
@@ -136,22 +136,22 @@
 //     stderr.writeln(e.toString());
 //   }
 // }
-//
+
 // String _uniformizeKey(String key) {
 //   key = key.trim().replaceAll('\n', '').toLowerCase();
 //   return key;
 // }
-//
+
 // //Localization Model
 // class LocalizationModel {
 //   final String language;
 //   final List<PhraseModel> phrases;
-//
+
 //   LocalizationModel({
 //     required this.language,
 //     required this.phrases,
 //   });
-//
+
 //   factory LocalizationModel.fromMap(Map data) {
 //     return LocalizationModel(
 //       language: data['language'],
@@ -159,26 +159,26 @@
 //           (data['phrases'] as List).map((v) => PhraseModel.fromMap(v)).toList(),
 //     );
 //   }
-//
+
 //   Map<String, dynamic> toJson() => {
 //         "language": language,
 //         "phrases": List<dynamic>.from(phrases.map((x) => x.toJson())),
 //       };
 // }
-//
+
 // class PhraseModel {
 //   String key;
 //   String phrase;
-//
+
 //   PhraseModel({required this.key, required this.phrase});
-//
+
 //   factory PhraseModel.fromMap(Map data) {
 //     return PhraseModel(
 //       key: data['key'],
 //       phrase: data['phrase'] ?? '',
 //     );
 //   }
-//
+
 //   Map<String, dynamic> toJson() => {
 //         "key": key,
 //         "phrase": phrase,
