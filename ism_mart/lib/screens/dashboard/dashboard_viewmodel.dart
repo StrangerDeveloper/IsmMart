df190533fe0c5b9aebe189663f225316b82b2533
Hasnain-Mirrani07 <hmirrani3@gmail.com>
2023-08-17 18:16:09 +0500

    firebase cli connected

--- ism_mart/lib/screens/dashboard/dashboard_viewmodel.dart
+++ ism_mart/lib/screens/dashboard/dashboard_viewmodel.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
@@ -6,25 +5,30 @@ import 'package:ism_mart/exports/exports_utils.dart';
 import 'package:ism_mart/helper/global_variables.dart';
 import 'package:ism_mart/helper/languages/translations_key.dart' as langKey;
 
-class DashboardViewModel extends GetxController{
-
+class DashboardViewModel extends GetxController {
   RxString currentAddress = ''.obs;
   Position? currentPosition;
 
-  getAddressFromPosition(Position position) async{
-    await placemarkFromCoordinates(currentPosition!.latitude, currentPosition!.longitude).then((List<Placemark> placemarks) {
-      Placemark place = placemarks[0];
-        currentAddress.value = "${place.locality}, " + "${place.country}";
+  Future<void> getCurrentLocation() async {
+    GlobalVariable.showLoader.value = true;
+    final hasPermissions = await handleLocationPermission();
+    if (!hasPermissions) {
       GlobalVariable.showLoader.value = false;
-        if(currentAddress.value.isNotEmpty) {
-          Get.toNamed(Routes.chatScreen, arguments: {"location": currentAddress.value});
-        } else{
-          AppConstant.displaySnackBar(langKey.errorTitle.tr, 'Encountered error while trying to fetch location. Try Again');
-        }
-    });
+      return;
+    } else {
+      await Geolocator.getCurrentPosition(
+              desiredAccuracy: LocationAccuracy.high)
+          .then((Position position) {
+        currentPosition = position;
+        getAddressFromPosition(currentPosition!);
+      }).catchError((e) {
+        GlobalVariable.showLoader.value = false;
+        AppConstant.displaySnackBar(langKey.errorTitle.tr, e);
+      });
+    }
   }
 
-  Future<bool> handleLocationPermission()async {
+  Future<bool> handleLocationPermission() async {
     bool serviceEnabled = false;
     LocationPermission permission;
 
@@ -38,31 +42,35 @@ class DashboardViewModel extends GetxController{
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        AppConstant.displaySnackBar(langKey.errorTitle.tr, 'Location access denied');
+        AppConstant.displaySnackBar(
+            langKey.errorTitle.tr, 'Location access denied');
         return false;
       }
     } else if (permission == LocationPermission.deniedForever) {
-      AppConstant.displaySnackBar(langKey.errorTitle.tr, 'Location access permanently denied');
+      AppConstant.displaySnackBar(
+          langKey.errorTitle.tr, 'Location access permanently denied');
       return false;
     }
     return true;
   }
 
-  Future<void> getCurrentLocation()async{
-    GlobalVariable.showLoader.value = true;
-    final hasPermissions = await handleLocationPermission();
-    if(!hasPermissions){
+  getAddressFromPosition(Position position) async {
+    await placemarkFromCoordinates(
+            currentPosition!.latitude, currentPosition!.longitude)
+        .then((List<Placemark> placemarks) {
+      Placemark place = placemarks[0];
+      currentAddress.value = "${place.locality}, " + "${place.country}";
       GlobalVariable.showLoader.value = false;
-      return;
-    } else{
-      await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high).then((Position position) {
-          currentPosition = position;
-        getAddressFromPosition(currentPosition!);
-      }).catchError((e){
-        GlobalVariable.showLoader.value = false;
-        AppConstant.displaySnackBar(langKey.errorTitle.tr, e);
-      });
-    }
+      if (currentAddress.value.isNotEmpty) {
+        Get.toNamed(Routes.chatScreen,
+            arguments: {"location": currentAddress.value});
+      } else {
+        AppConstant.displaySnackBar(langKey.errorTitle.tr,
+            'Encountered error while trying to fetch location. Try Again');
+      }
+    }).catchError((e) {
+      AppConstant.displaySnackBar(langKey.errorTitle.tr, e);
+      GlobalVariable.showLoader.value = false;
+    });
   }
-
 }
